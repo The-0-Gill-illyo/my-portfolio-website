6d8aef275033823c7dfa35701421de6c36bc012f
The-0-Gill-illyo <theogill1369@gmail.com>
2023-01-04 12:18:08 -0500

    transfed project images that will be displayed + Created files/folders for to store the image code + Changed the tab name to display Theo's Developer Portfolio

---
+++ theos-web-portfolio/src/components/HeroImg.js
@@ -0,0 +1,16 @@
+//Page Imports
+import "./HeroImgStyles.css";
+
+//Other Imports
+import React from 'react'
+
+const HeroImg = () => {
+  return <div className="hero">
+    <div className="mask">
+
+    </div>
+  </div>;
+  
+};
+
+export default HeroImg;
--- theos-web-portfolio/src/routes/Home.js
+++ theos-web-portfolio/src/routes/Home.js
@@ -1,13 +1,15 @@
-//Page imports
+//Page Imports
 import Navbar from '../components/Navbar';
+import HeroImg from "../components/HeroImg";
 
-//Other imports
+//Other Imports
 import React from 'react';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <HeroImg />
     </div>
   )
 }
