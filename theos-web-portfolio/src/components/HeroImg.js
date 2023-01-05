2eca74e3842e9cf86f48adae6bc479c935c1388d
The-0-Gill-illyo <theogill1369@gmail.com>
2023-01-05 11:32:06 -0500

    Finished setting up my Home Page with clickable/responsive links, set up the stlying and layout for Home page

--- theos-web-portfolio/src/components/HeroImg.js
+++ theos-web-portfolio/src/components/HeroImg.js
@@ -1,13 +1,23 @@
 //Page Imports
 import "./HeroImgStyles.css";
+import HomeImage from "../assests/BlackManWomanDevelopers.jpg";
 
 //Other Imports
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const HeroImg = () => {
   return <div className="hero">
     <div className="mask">
-
+    <img className="intro-img" src={HomeImage} alt="HomeImage" />
+    </div>
+    <div className="content">
+      <p>Hi I'm Theo a freelance</p>
+      <h1>Web Developer, specializing in React!</h1>
+      <div>
+        <Link to="/projects" className="btn">Projects</Link>
+        <Link to="/contact" className="btn btn-light">Contact</Link>
+      </div>
     </div>
   </div>;
   
