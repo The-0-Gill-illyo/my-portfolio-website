d230306b0368bdb18345df9f83312f0a6eee4c73
The-0-Gill-illyo <theogill1369@gmail.com>
2023-01-13 18:08:11 -0500

    Imported images to use on About page + created logic to isolate the images + installed bootstap

--- theos-web-portfolio/src/components/AboutContent.js
+++ theos-web-portfolio/src/components/AboutContent.js
@@ -1,13 +1,34 @@
 //Page Imports
 import "./AboutContentStyles.css";
+import ReactImage1 from "../assests/ReactImage1.jpg";
+import ReactImage2 from "../assests/ReactImage2.jpg";
 
 //Other Imports
-
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const AboutContent = () => {
   return (
-    <div>AboutContent</div>
+    <div className="about">
+      <div className="left">
+        <h1>Who is Theo?</h1>
+        <p>I am Full-Stack Developer and aspiring self-taught Data Analysis, 
+          who makes resposive React applications and websites!</p>
+          <Link to="/contact">
+            <button className="btn">Contact</button>
+          </Link>
+      </div>
+      <div className="right">
+        <div className="img-container">
+          <div className="img-stack top">
+            <img src={ReactImage1} className="img" alt="React Symbol" />
+          </div>
+          <div className="img-stack bottom">
+            <img src={ReactImage2} className="img" alt="Black Developer" />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
